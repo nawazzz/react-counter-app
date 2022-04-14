53c5169aeed5ffedd9cd3ab620544e2c3daaa159
unknown <khan.nawaz790@gmail.com>
2022-04-14 16:31:57 +0200

    create button containers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@
 import React from 'react';
 import './App.scss';
 import cart from './cart.svg'
+import refreshIcon from './refresh.svg'
+import recycleIcon from './recycle.svg'
+import plusIcon from './plus.svg'
+import minusIcon from './minus.svg'
+import trashIcon from './trash.svg'
 
 class App extends React.Component {
   constructor(props) {
@@ -28,6 +33,33 @@ class App extends React.Component {
           <div>
             <h6>items</h6>
           </div>
+        </div>
+        <div className='countOperationContainer'>
+          <div className='buttonsContainer'>
+            <div className='refreshButton'>
+              <img src={refreshIcon} alt={"refresh"}/>
+            </div>
+            <div className='recycleButton'>
+              <img src={recycleIcon} alt={"recycle"}/>
+            </div>
+          </div>
+          <div className='heroContainer'>
+            <div className='buttonContainerOne'>
+              {"Zero"}
+            </div>
+            <div className='buttonContainerTwo'>
+              <img src={plusIcon} alt={"plus sign"}/>
+            </div>
+            <div className='buttonContainerThree'>
+              <img src={minusIcon} alt={"minus sign"}/>
+            </div>
+            <div className='buttonContainerFour'>
+              <img src={trashIcon} alt={"delete sign"}/>
+            </div>
+          </div>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
       </div>
     );
