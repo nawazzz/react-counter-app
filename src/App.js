e0b5545f87f0218d0ab38d689e3e6214dde83522
unknown <khan.nawaz790@gmail.com>
2022-04-14 14:33:04 +0200

    create counter app files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import './App.scss';
+import cart from './cart.svg'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className='mainContainer'>
+        <div className='headerContainer'>
+          <h1>Counter App</h1>
+        </div>
+        <div className='totalCountContainer'>
+          <div style={{width: "20px"}}>
+            <img src={cart} alt="cart" style={{width: "100%"}} />
+            {/* <FontAwesomeIcon icon="fa-solid fa-cart-shopping" /> */}
+          </div>
+          <div className=''>
+              <h6>0</h6>
+          </div>
+          <div>
+            <h6>items</h6>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
